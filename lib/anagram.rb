3980b067ef9ffd87b2484290a9b79fcfeb629740
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-25 01:16:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,21 +14,7 @@ class Anagram
     end
   end
 
-
 end
 
 listen = Anagram.new("listen")
-#listen.match(%w(enlists))
 listen.match(%w(enlists google inlets banana))
-
-# => ["inlets"]
-
-#test = %w(enlists google inlets banana)
-#word = "listen"
-
-
-#if (word.split("").sort) == (test[2].split("").sort)
-#  puts "test is true"
-#else
-#  puts "test is false"
-#end
